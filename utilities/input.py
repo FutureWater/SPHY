a78924b5a537648b2f476c4ff5d60eee685de5d4
Joris Eekhout <joriseekhout@gmail.com>
2022-05-04 10:01:46 +0200

    add input utility to read parameters and maps

---
+++ utilities/input.py
@@ -0,0 +1,31 @@
+# The Spatial Processes in HYdrology (SPHY) model:
+# A spatially distributed hydrological model 
+# Copyright (C) 2013-2019  FutureWater
+# Email: sphy@futurewater.nl
+#
+# Authors (alphabetical order):
+# P. Droogers, J. Eekhout, W. Immerzeel, S. Khanal, A. Lutz, G. Simons, W. Terink
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#-Function to read maps and float input
+def input(self, config, pcr, name, module, variable, empty = None):
+    try:
+        setattr(self, name, self.ones * config.getfloat(module, variable))
+    except:
+        try:
+            setattr(self, name, pcr.readmap(self.inpath + config.get(module, variable)))
+        except:
+            setattr(self, name, self.ones * empty)
