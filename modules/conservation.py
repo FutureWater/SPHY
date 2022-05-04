b619e8046448bd2a55f7ee71ffa67f329d2460e1
Joris Eekhout <joriseekhout@gmail.com>
2022-05-04 14:59:44 +0200

    update conservation module

--- modules/conservation.py
+++ modules/conservation.py
@@ -26,18 +26,18 @@ def init(self, pcr, config):
         self.conservationFLAG = 1
 
         #-read conservation measures map
-        self.input.input(self, config, pcr, 'conservationMeasures', 'EROSION', 'conservationMeasures', 0)
+        self.input.input(self, config, pcr, 'conservationMeasures', 'SEDIMENT', 'conservationMeasures', 0)
 
         #-read table with conservation input parameters per conservation measure class
         pcr.setglobaloption('matrixtable')
-        CONSERVATION_table = self.inpath + config.get('EROSION', 'CONSERVATION_table')
+        CONSERVATION_table = self.inpath + config.get('SEDIMENT', 'CONSERVATION_table')
         self.NoElements_conservation = pcr.lookupscalar(CONSERVATION_table, 1, self.conservationMeasures)
         self.Diameter_conservation = pcr.lookupscalar(CONSERVATION_table, 2, self.conservationMeasures)
         self.n_table_conservation = pcr.lookupscalar(CONSERVATION_table, 3, self.conservationMeasures)
         pcr.setglobaloption('columntable')
 
         #-Determine flow velocity for conservation measures
-        self.n_veg_TC_conservation = self.roughness.manningVegetation(self.d_field, self.Diameter_conservation, self.NoElements_conservation)
+        self.n_veg_TC_conservation = self.mmf.manningVegetation(self, pcr, self.d_field, self.Diameter_conservation, self.NoElements_conservation)
         self.n_veg_TC_conservation = pcr.ifthenelse(self.n_table_conservation > 0, self.n_table_conservation, self.n_veg_TC_conservation)
         self.n_TC_conservation = (self.n_soil**2 + self.n_veg_TC_conservation**2)**0.5
         self.v_TC_conservation = self.mmf.FlowVelocity(self, pcr, self.n_TC_conservation, self.d_TC)
